cb8f44cb4cc6c8a2fb0d08847637132b09be858e
jakkii <licc06ca@gmail.com>
2025-10-09 13:07:11 -0400

    Fix reset condition and reorder FF connections

--- lab4/part1.sv
+++ lab4/part1.sv
@@ -19,7 +19,7 @@ module ff (
         end
     end
     always_ff @(posedge clk) begin
-        if (!rst_n)
+        if (rst_n)
             q <= 1'b0;
         else
             q <= shift_in;
@@ -52,7 +52,7 @@ module ff_0 (
         end
     end
     always_ff @(posedge clk) begin
-        if (!rst_n)
+        if (rst_n)
             q <= 1'b0;
         else
             q <= shift_in;
@@ -63,9 +63,8 @@ module part1 (input logic clock, reset, ParallelLoadn, RotateRight, ASRight,
                 input logic [3:0] Data_IN, 
                 output logic [3:0] Q);
             // ASR only affects the first FF
-    ff_0 ff0 (.clk(clock), .rst_n(reset), .d(Data_IN[0]), .loadn(ParallelLoadn), .LoadLeft(RotateRight), .right(Q[2]), .left(Q[0]), .asr(ASRight), .q(Q[3]));
-    ff ff1 (.clk(clock), .rst_n(reset), .d(Data_IN[1]), .loadn(ParallelLoadn), .LoadLeft(RotateRight), .right(Q[1]), .left(Q[3]), .q(Q[2]));
-    ff ff2 (.clk(clock), .rst_n(reset), .d(Data_IN[2]), .loadn(ParallelLoadn), .LoadLeft(RotateRight), .right(Q[0]), .left(Q[2]), .q(Q[1]));
-    ff ff3 (.clk(clock), .rst_n(reset), .d(Data_IN[3]), .loadn(ParallelLoadn), .LoadLeft(RotateRight), .right(Q[3]), .left(Q[1]), .q(Q[0]));
+    ff_0 ff0 (.clk(clock), .rst_n(reset), .d(Data_IN[3]), .loadn(ParallelLoadn), .LoadLeft(RotateRight), .right(Q[2]), .left(Q[0]), .asr(ASRight), .q(Q[3]));
+    ff ff1 (.clk(clock), .rst_n(reset), .d(Data_IN[2]), .loadn(ParallelLoadn), .LoadLeft(RotateRight), .right(Q[1]), .left(Q[3]), .q(Q[2]));
+    ff ff2 (.clk(clock), .rst_n(reset), .d(Data_IN[1]), .loadn(ParallelLoadn), .LoadLeft(RotateRight), .right(Q[0]), .left(Q[2]), .q(Q[1]));
+    ff ff3 (.clk(clock), .rst_n(reset), .d(Data_IN[0]), .loadn(ParallelLoadn), .LoadLeft(RotateRight), .right(Q[3]), .left(Q[1]), .q(Q[0]));
 endmodule
-        
